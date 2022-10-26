abfa9a5ad703429e8eb3d65a7f45be58309ba785
Chili0xFF <75846282+Chili0xFF@users.noreply.github.com>
2022-10-26 22:39:05 +0200

    lab04
    implemented result_t structure, added fitting function and implemented display methods

--- lab04/main.cpp
+++ lab04/main.cpp
@@ -8,6 +8,7 @@ std::random_device rd;
 std::mt19937 mt_generator(rd());
 using chromosome_t = std::vector<int>;
 using population_t = std::vector<chromosome_t>;
+using result_t = std::vector<std::vector<double>>;
 auto beal_f = [](double x, double y) {
     double firstPart = pow((1.5-x+(x*y)),2);
     double secondPart = pow(2.25-x+(x*pow(y,2)),2);
@@ -29,10 +30,21 @@ population_t populate(int pop_size, int chrom_size){
 double translate(chromosome_t chromosome){
     using namespace std;
     double result=0;
-    reverse(chromosome.begin(),chromosome.end());
-    for(int i=0;i<chromosome.size();i++){
-        result+=(chromosome.at(i)*pow(2,i));                            //DODAJ TUTAJ ABY MOGŁO DAWAĆ WARTOŚCI UJEMNE i ułamki, podzielic translate na x i y w połwoei
+    bool flagNegative = false;
+    if(chromosome.at(0)==1)flagNegative=true;
+
+    double twos=1;
+    for(int i=2;i>=1;--i){
+        result+=(chromosome.at(i)*twos);                            //DODAJ TUTAJ ABY MOGŁO DAWAĆ WARTOŚCI UJEMNE i ułamki, podzielic translate na x i y w połwoei
+        twos *= 2.0;
     }
+    twos = 2;
+    for (int i = 3; i < chromosome.size(); ++i) {
+        result += (chromosome.at(i)/twos);
+        twos *= 2.0;
+    }
+
+    if(flagNegative)result*=-1;
     return result;
 }
 auto genetic_algorithm = [](
@@ -62,35 +74,51 @@ auto genetic_algorithm = [](
     return population;
 };
 
-std::vector<double> fitness_function(population_t population){
+result_t fitness_function(population_t population){
 
 
-    std::vector<double> top10;
+    result_t top10;
     std::vector<double> translated;
-    std::vector<double> results;
+    result_t results;
     for(chromosome_t chrom : population){
-        double a = fmod(translate(chrom),4.5);
+        chromosome_t chrom_a, chrom_b;
+        for (int i = 0; i < (chrom.size()/2)-1; ++i) {
+            chrom_a.push_back(chrom.at(i));
+        }
+        for (int i=chrom.size()/2;i<chrom.size()-1;i++){
+            chrom_b.push_back(chrom.at(i));
+        }
+        double a = translate(chrom_a);
+        double b = translate(chrom_b);
         translated.push_back(a);
+        translated.push_back(b);
+        //std::cout<<"A: "<<a<<" B: "<<b<<std::endl;
+
     }
 
-    for(int i=1;i<translated.size();i+=2){
-        results.push_back(
-                (beal_f(translated.at(i),translated.at(i-1)))
-                );
-        //std::cout<<results.back()<<std::endl;
+    for(int i=0;i<translated.size();i+=2){
+        std::vector<double> temp={
+                (1/(beal_f(translated.at(i),translated.at(i+1)))),
+                (translated.at(i)),
+                (translated.at(i+1))
+        };
+        results.push_back(temp);
+        //std::cout<<"Beal:"<<1/results.back()[0]<<" Score: "<<results.back()<<std::endl;
     }
-
+using namespace std;
     for(int i=0;i<10;i++){
-        int best=0;
+        vector<double> best={0,0,0};
         int bestId=0;
+
         for(int j=0;j<results.size();j++){
-            if(best<results.at(j)) {
-                best = results.at(j);
-                bestId = j;
+            //cout<<j+1<<". Score: "<<results.at(j).at(0)<<" X: "<<results.at(j).at(1)<<" Y: "<<results.at(j).at(2)<<endl;
+            if(results.at(j).at(0)>best.at(0)){
+                best=results.at(j);
+                bestId=j;
             }
         }
         top10.push_back(best);
-        results.at(bestId)=0;
+        results.at(bestId)={0,0,0};
     }
     return top10;
 }
@@ -118,7 +146,7 @@ int main() {
     };
 
     using namespace std;
-    population_t population = populate(100,100+(23049%10)*2);
+    population_t population = populate(10000,100+(23049%10)*2);
 //    for (chromosome_t chromosome: result) {
 //        cout << "[";
 //        for (int p: chromosome) {
@@ -133,14 +161,22 @@ int main() {
 //                                    selection_empty, 1.0,
 //                                    crossover_empty,
 //                                    0.01, mutation_empty);
-    for(chromosome_t chrom : population){
-        cout<<translate(chrom)<<endl;
-    }
+//    for(chromosome_t chrom : population){
+//        cout<<translate(chrom)<<endl;
+//    }
     auto result = fitness_function(population);
-    cout<<"top10\n";
-    for(double a:result){
-        cout<<a<<endl;
+    cout<<"top10 score\n";
+    for(int i=0;i<result.size();i++){
+        cout<<i+1<<". Score: "<<result.at(i).at(0)<<" X: "<<result.at(i).at(1)<<" Y: "<<result.at(i).at(2)<<endl;
+    }
+    while(true){
+        double a,b;
+        cout<<"a: ";
+        cin>>a;
+        if(a==0.0)return 0;
+        cout<<"b: ";
+        cin>>b;
+        cout<<beal_f(a,b)<<endl;
     }
 
-    return 0;
 }
